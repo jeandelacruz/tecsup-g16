f5a4d1b790e367320ed8dcc079a84fd735bba6da
Jeancarlos De la Cruz Criollo <jdelacruzcr94@gmail.com>
2022-08-23 21:41:47 -0500

    Martes 23/08 Segunda parte

---
+++ Semana 2/Martes/ejercicios.py
@@ -0,0 +1,35 @@
+## 1º Crea una función que te permite ingresar la altura y el ancho de un rectangulo
+## y que este mismo lo dibuje con el signo *, ejemplo:
+## altura: 5
+## ancho: 4
+## Resultado:
+# ****
+# ****
+# ****
+# ****
+# ****
+# for y range
+
+# 2ª Crea una función que reciba la altura de un triangulo rectangulo
+# este mismo va a pintar un triangulo pero invertido, ejemplo:
+# altura: 4
+# ****
+# ***
+# **
+# *
+# for y range(inicio, fin, pasos)
+
+# 3º Crea un programa que reciba un numero entero,
+# ese mismo debemos de hacer que llegue hasta 1, con la seria de Collatz
+# si el numero es par, se divide entre dos
+# si el numero es impar, se multiplica por 3 y se suma 1
+# la serie termina cuando llega al numero 1
+# Ejemplo: 
+# Numero: 19
+# 19 58 29 88 44 ......... 1
+# while if 
+
+numero = 19
+
+while numero != 1:
+    pass
