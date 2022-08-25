4f14b5bd87cceca7cc7b857fb3a44b271980dc2c
Jeancarlos De la Cruz Criollo <jdelacruzcr94@gmail.com>
2022-08-24 22:49:10 -0500

    Miercoles 24/08 Parte final

--- Flask/main.py
+++ Flask/main.py
@@ -1,5 +1,4 @@
-import ssl
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__) # main.py
 
@@ -37,6 +36,39 @@ def userGetById(id):
     for user in users:
         if user['id'] == id:
             return user, 200
+    
+    return {
+        'message': 'El usuario ingresado no existe'
+    }, 404
+
+@app.route('/users', methods=['POST'])
+def userCreate():
+    body = request.get_json()
+    users.append(body)
+    return {
+        'message': 'Usuario creado con exito'
+    }, 201
+
+@app.route('/users/<int:id>', methods=['PUT'])
+def userUpdate(id):
+    body = request.get_json()
+    
+    for user in users:
+        if user['id'] == id:
+            user['name'] = body['name']
+            user['last_name'] = body['last_name']
+            return user, 200
+    
+    return {
+        'message': 'El usuario ingresado no existe'
+    }, 404
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def userDelete(id):
+    for index, value in enumerate(users):
+        if value['id'] == id:
+            users.pop(index)
+            return users, 200
     
     return {
         'message': 'El usuario ingresado no existe'
